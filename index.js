44d74501bcdc59560f0e9d6e8c60cb1236960e40
Abdi Deresa <Abdishegute@gmail.com>
2024-09-04 10:40:17 -0700

    added js

---
+++ index.js
@@ -0,0 +1,23 @@
+//Counter Program
+const decreaBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+
+const countLabel = document.getElementById("countLabel");
+
+let count = 0;
+
+decreaBtn.onclick = function () {
+  count--;
+  countLabel.textContent = count;
+};
+
+increaseBtn.onclick = function () {
+  count++;
+  countLabel.textContent = count;
+};
+
+resetBtn.onclick = function () {
+  count = 0;
+  countLabel.textContent = count;
+};
